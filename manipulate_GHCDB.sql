af054351cfab52a5c6faaa465ae7e2470b9f86ff
Hyunseung Choi <bocalist@gmail.com>
2024-02-12 14:15:55 -0700

    added activity and lab for minipulating data

--- manipulate_GHCDB.sql
+++ manipulate_GHCDB.sql
@@ -1,6 +1,6 @@
 -- This script manipulates the Grand Hill Collage DB with required data
-
-spool C:/dev/sait/db/populate_GHCDB_output.txt
+-- @C:/dev/sait/db/manipulate_GHCDB.sql
+spool C:/dev/sait/db/manipulate_GHCDB_output.txt
 set echo on
 
 -- Add a new department
@@ -32,13 +32,30 @@ INSERT INTO GHC_EXPERTISE (faculty_id, course_code)
 
 COMMIT;
 
-UPDATE GHC_COURSE SET course_code='APHY302' WHERE COURSE_CODE='APHY203';
+INSERT INTO GHC_COURSE (course_code, course_title, credits)
+    VALUES ('APHY302', 'Nebula', 5);
 
+INSERT INTO GHC_EXPERTISE (faculty_id, course_code)
+    VALUES (1001, 'APHY302');
+
+COMMIT;
+
+DELETE FROM GHC_EXPERTISE WHERE COURSE_CODE='APHY203';
+COMMIT;
+
+DELETE FROM GHC_COURSE WHERE COURSE_CODE='APHY203';
+COMMIT;
+
+DELETE FROM GHC_EXPERTISE WHERE FACULTY_ID=1001;
 COMMIT;
 
 DELETE FROM GHC_FACULTY WHERE FACULTY_Id=1001;
-
 COMMIT;
+
+SELECT * FROM GHC_FACULTY;
+SELECT * FROM GHC_EXPERTISE;
+SELECT * FROM GHC_DEPARTMENT;
+SELECT * FROM GHC_COURSE;
 
 
 spool off
